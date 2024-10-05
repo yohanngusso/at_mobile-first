fdfe93b50b9215b6e8a55eb2e953c4301322c19e
Yohann Gusso <yohann.gusso@hotmail.com>
2024-10-04 21:40:20 -0300

    fix: src images

--- src/AT/01/Menu.jsx
+++ src/AT/01/Menu.jsx
@@ -1,10 +1,12 @@
 import './menu.css';
+import Image1 from '../../assets/images/logo.png';
+import Image2 from '../../assets/images/menu-icon.png';
 
 export default function Menu() {
     return (
         <nav className='menu'>
             <div className='menu-logo'>
-                <img src='logo.png' alt='Logo' />
+                <img src={Image1} alt='Logo' />
             </div>
             <div className='menu-items'>
                 <a href='#'>Produtos</a>
@@ -12,7 +14,7 @@ export default function Menu() {
                 <a href='#'>Contato</a>
             </div>
             <div className='menu-icon'>
-                <img src='menu-icon.png' alt='Menu Icon' />
+                <img src={Image2} alt='Menu Icon' />
             </div>
         </nav>
     );
